83fd8470351ca561c88221619c56746a9e5842f1
Alix <fullertonalix0@gmail.com>
2022-12-27 08:48:08 -0500

    Removing unused workflows (#9)
    * Removing unused workflows
    
    * Prepping for helm build

--- cicd/build-containers.sh
+++ cicd/build-containers.sh
@@ -1,25 +1,24 @@
 # Builds Development Containers
 #
 #
-PIGNUS_DIR="/home/kube/pignus"
-PIGNUS_SRC="/home/kube/pignus/src"
+PIGNUS_DIR="/Users/alix/Programming/repos/pignus/"
 PIGNUS_TAG="0.0.1"
 
 # Add Source
-cp -r /home/kube/repos/pignus/src/ /home/kube/repos/pignus/docker/pignus-api/
-rm -rf /home/kube/repos/pignus/docker/pignus-api/src/build
-rm -rf /home/kube/repos/pignus/docker/pignus-api/src/dist
-rm -rf /home/kube/repos/pignus/docker/pignus-api/src/pignus.egg-info
+cp -r ${PIGNUS_DIR}/src ${PIGNUS_DIR}/docker/pignus-api/
+rm -rf ${PIGNUS_DIR}/docker/pignus-api/src/build
+rm -rf ${PIGNUS_DIR}/docker/pignus-api/src/dist
+rm -rf ${PIGNUS_DIR}/docker/pignus-api/src/pignus.egg-info
 
 # Add Tests
-cp -r /home/kube/repos/pignus/tests/ /home/kube/repos/pignus/docker/pignus-api/
+cp -r ${PIGNUS_DIR}/tests/ ${PIGNUS_DIR}/docker/pignus-api/
 
 # cp -r /home/kube/repos/pignus/test/ /home/kube/repos/pignus/docker/pignus-api
-cd /home/kube/repos/pignus/docker/pignus-api
+cd ${PIGNUS_DIR}/docker/pignus-api
 docker build -t politeauthority/pignus-api:${PIGNUS_TAG} .
 
 # Remove the source
-rm -rf /home/kube/repos/pignus/docker/pignus-api/src
-rm -rf /home/kube/repos/pignus/docker/pignus-api/tests
+rm -rf ${PIGNUS_DIR}/docker/pignus-api/src
+rm -rf ${PIGNUS_DIR}/docker/pignus-api/tests
 
 echo "Build complete!"
