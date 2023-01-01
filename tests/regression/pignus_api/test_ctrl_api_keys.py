82be985ab61efdff17e707571e6ff6303d4bf6db
Alix Fullerton <fullertonalix0@gmail.com>
2022-12-31 18:38:09 -0700

    adding stupid unneeded test

--- tests/regression/pignus_api/test_ctrl_api_keys.py
+++ tests/regression/pignus_api/test_ctrl_api_keys.py
@@ -35,5 +35,22 @@ class TestApiApiKeys:
         assert "objects" in response_json
         assert "object_count" in response_json
 
+    def test__api_keys_get2(self):
+        """Tests the ApiKeys collections through the Pignus Api
+        GET /api-keys
+        """
+        request_args = {
+            "headers": HEADERS,
+            "method": "GET",
+            "url": "%s/api-keys" % PIGNUS_API_URL,
+        }
+
+        response = requests.request(**request_args)
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["object_type"] == "api_key"
+        assert "objects" in response_json
+        assert "object_count" in response_json
+
 
 # End File: pignus/tests/regression/pignus_api/test_ctrl_images.py
